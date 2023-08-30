bb854fdea5b43edaac07a8381034a63596ddafc5
Marcos Candeia <marrcooos@gmail.com>
2023-08-30 15:42:22 -0300

    Use dotenv to specify sitename (#4)
    * Use dotenv to specify sitename
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * Commit .env file
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    ---------
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- runtime.ts
+++ runtime.ts
@@ -1,4 +1,4 @@
-import { proxy } from "$live/clients/withManifest.ts";
+import { proxy } from "deco/clients/withManifest.ts";
 import type { Manifest } from "./manifest.gen.ts";
 import type { Manifest as ManifestVNDA } from "apps/vnda/manifest.gen.ts";
 import type { Manifest as ManifestVTEX } from "apps/vtex/manifest.gen.ts";
