044bae3802e0d002dbe89d99a4d01eaa86a51761
Agliardi Digital <contato@agliardidigital.com.br>
2023-09-14 16:05:15 -0300

    linx integration

--- components/header/Buttons/Cart/linx.tsx
+++ components/header/Buttons/Cart/linx.tsx
@@ -3,17 +3,16 @@ import Button from "./common.tsx";
 
 function CartButton() {
   const { loading, cart } = useCart();
-  const cartSize = cart.value?.Basket?.Items?.length || 0;
-  const products = cart.value?.Basket?.Items;
-  const coupon = cart.value?.Basket?.Coupons?.[0]?.Code;
+  const { total, products, coupon } = cart.value;
 
   return (
     <Button
       currency="BRL"
       loading={loading.value}
-      total={cartSize}
-      items={(products ?? []).map((item, index) =>
-        itemToAnalyticsItem(item, coupon, index)
+      total={total}
+      items={(products ?? []).map(
+        (item, index) =>
+          itemToAnalyticsItem({ ...item!, coupon: coupon ?? "" }, index),
       )}
     />
   );
