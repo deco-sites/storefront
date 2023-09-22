3eea10d39c0241d357a11d57b06102d754f8784e
Agliardi Digital <contato@agliardidigital.com.br>
2023-09-22 16:22:09 -0300

    login

--- components/login/Login.tsx
+++ components/login/Login.tsx
@@ -1,5 +1,7 @@
+import { itemToAnalyticsItem } from "apps/linx/hooks/useLogin.ts";
 import Button from "$store/components/ui/Button.tsx";
 import { useId } from "$store/sdk/useId.ts";
+
 
 export interface Props {
   placeholderName: string;
@@ -13,12 +15,20 @@ function Login({
   textButton
 }: Props) {
     const id = useId();
+    const key = useId();
+    const password = useId();
     const action = "/s";
+    const objLogin = {
+      login: {
+        key,
+        password
+      }
+    }
   return (
     <>
      <form id={id} action={action} class="join">
-        <input name="Key" placeholder={placeholderName}></input>
-        <input name="Password" placeholder={placeholderPassword}></input>
+        <input name="Key" id={key} placeholder={placeholderName}></input>
+        <input name="Password" id={password} placeholder={placeholderPassword}></input>
         <button>{textButton}</button>
         <Button
           type="submit"
