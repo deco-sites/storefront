f67d7a742f36ee53971288856c52d76f3556d5b1
gimenes <tlgimenes@gmail.com>
2023-09-25 15:52:35 -0300

    debouce according to api speeds

--- sdk/useSuggestions.ts
+++ sdk/useSuggestions.ts
@@ -2,43 +2,46 @@ import { signal } from "@preact/signals";
 import type { Suggestion } from "apps/commerce/types.ts";
 import type { Resolved } from "deco/engine/core/resolver.ts";
 import { useCallback } from "preact/compat";
-import { debounce } from "std/async/debounce.ts";
 import { invoke } from "../runtime.ts";
 
 const payload = signal<Suggestion | null>(null);
 const loading = signal<boolean>(false);
 
-const debounced = debounce(
-  async (
-    query: string,
-    { __resolveType, ...extraProps }: Resolved<Suggestion | null>,
-  ) => {
-    try {
-      // Figure out a better way to type this loader
-      // deno-lint-ignore no-explicit-any
-      const invokePayload: any = {
-        key: __resolveType,
-        props: { query, ...extraProps },
-      };
-      payload.value = await invoke(invokePayload) as Suggestion | null;
-    } catch (error) {
-      console.error(
-        "Something went wrong while fetching suggestions \n",
-        error,
-      );
-    } finally {
-      loading.value = false;
-    }
-  },
-  150,
-);
+let queue = Promise.resolve();
+let latestQuery = "";
+
+const doFetch = async (
+  query: string,
+  { __resolveType, ...extraProps }: Resolved<Suggestion | null>,
+) => {
+  // Debounce query to API speed
+  if (latestQuery !== query) return;
+
+  try {
+    // Figure out a better way to type this loader
+    // deno-lint-ignore no-explicit-any
+    const invokePayload: any = {
+      key: __resolveType,
+      props: { query, ...extraProps },
+    };
+    payload.value = await invoke(invokePayload) as Suggestion | null;
+  } catch (error) {
+    console.error(
+      "Something went wrong while fetching suggestions \n",
+      error,
+    );
+  } finally {
+    loading.value = false;
+  }
+};
 
 export const useSuggestions = (
   loader: Resolved<Suggestion | null>,
 ) => {
   const setQuery = useCallback((query: string) => {
     loading.value = true;
-    debounced(query, loader);
+    latestQuery = query;
+    queue = queue.then(() => doFetch(query, loader));
   }, [loader]);
 
   return {
