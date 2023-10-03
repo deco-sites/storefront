3a4f147d3de19578e0e8bd94807aebed845c01e7
Augusto Barbosa <augustocbarbosa@gmail.com>
2023-10-03 10:09:58 -0300

    Fix lint

--- components/footer/BackToTop.tsx
+++ components/footer/BackToTop.tsx
@@ -1,12 +1,15 @@
 import Icon from "$store/components/ui/Icon.tsx";
+import { ButtonType, getButtonClasses } from "$store/components/ui/Types.tsx";
 
-export default function BackToTop({ content }: { content?: string }) {
+export default function BackToTop(
+  { children, style }: { children?: string; style?: ButtonType },
+) {
   return (
     <>
-      {content && (
-        <div class="w-full flex items-center justify-center">
-          <a href="#top" class="btn">
-            {content} <Icon id="ChevronUp" width={24} height={24} />
+      {children && (
+        <div class="w-full flex items-center justify-center mt-8 md:mt-0">
+          <a href="#top" class={`${getButtonClasses(style || {})} btn-sm`}>
+            {children} <Icon id="ChevronUp" width={24} height={24} />
           </a>
         </div>
       )}
---
+++ components/ui/SimpleContainer.tsx
@@ -0,0 +1,86 @@
+import type { VNode } from "preact";
+import {
+  ButtonType,
+  colorClasses,
+  Colors,
+  Layout as SectionLayout,
+  layoutClasses,
+  Section,
+  textColorClasses,
+  TextColors,
+} from "$store/components/ui/Types.tsx";
+import type { ImageWidget } from "apps/admin/widgets.ts";
+
+export type Layout = SectionLayout;
+
+export interface Style {
+  section?: Section;
+  content?: {
+    bgColor?: Colors;
+    bgImage?: ImageWidget;
+    textColor?: TextColors;
+  };
+  button?: ButtonType;
+}
+
+export interface Props {
+  layout?: Layout;
+  style?: Style;
+  children: VNode;
+}
+
+const DEFAULT_PROPS: Props = {
+  children: <></>,
+};
+
+export default function Container({ children, ...props }: Props) {
+  const { layout, style } = { ...DEFAULT_PROPS, ...props };
+
+  const sectionBgColor = style?.section?.bgColor || "Transparent";
+  const contentBgColor = style?.content?.bgColor || "Transparent";
+  const containerBgColorClasses = sectionBgColor
+    ? colorClasses[sectionBgColor]
+    : "";
+  const contentBgColorClasses = contentBgColor
+    ? colorClasses[contentBgColor]
+    : "";
+
+  const hasPadding =
+    (contentBgColor !== "Transparent" && sectionBgColor !== contentBgColor) ||
+    style?.content?.bgImage !== undefined;
+
+  return (
+    <div
+      class={`
+      ${containerBgColorClasses}
+      ${layoutClasses[layout?.sectionWidth || "Contained"]}
+      ${hasPadding ? "p-4 lg:p-16" : ""}
+      ${style?.section?.bgImage ? "bg-cover bg-center" : ""}`}
+      style={{
+        "background-image": style?.section?.bgImage
+          ? `url(${style?.section?.bgImage})`
+          : "",
+      }}
+    >
+      <div
+        class={`
+          gap-6 lg:gap-12
+          ${hasPadding ? "p-4 lg:p-10" : "px-4 py-8 lg:p-16"}
+          ${layoutClasses[layout?.innerContentWidth || "Contained"]}
+          ${contentBgColorClasses}
+          ${style?.content?.bgImage ? "bg-cover bg-center" : ""}
+          ${textColorClasses[style?.content?.textColor || "Auto"]}
+        `}
+        style={{
+          "background-image": style?.content?.bgImage
+            ? `url(${style?.content?.bgImage})`
+            : "",
+        }}
+      >
+        <div class={`flex flex-col`}>
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+}
