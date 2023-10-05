4df5b28ee0f31f5b55c0eeb0caae6b5b53b00b95
gimenes <tlgimenes@gmail.com>
2023-10-05 11:40:11 -0300

    wip

--- dev.ts
+++ dev.ts
@@ -6,3 +6,7 @@ import config from "./fresh.config.ts";
 
 // Generate manifest and boot server
 await dev(import.meta.url, "./main.ts", config);
+
+if (Deno.args.includes("build")) {
+  Deno.exit(0);
+}
