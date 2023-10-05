7a6036b8a0c0a14b6d9e435928f3ee605eb3af7e
gimenes <tlgimenes@gmail.com>
2023-10-05 08:26:16 -0300

    migrate fresh

--- dev.ts
+++ dev.ts
@@ -1,6 +1,8 @@
 #!/usr/bin/env -S deno run -A --watch
-import dev from "$fresh/dev.ts";
 import "https://deno.land/x/dotenv@v3.2.2/load.ts";
 
+import dev from "$fresh/dev.ts";
+import config from "./fresh.config.ts";
+
 // Generate manifest and boot server
-await dev(import.meta.url, "./main.ts");
+await dev(import.meta.url, "./main.ts", config);
--- main.ts
+++ main.ts
@@ -4,16 +4,7 @@
 /// <reference lib="esnext" />
 
 import { start } from "$fresh/server.ts";
-import plugins from "https://denopkg.com/deco-sites/std@1.22.0/plugins/mod.ts";
-import partytownPlugin from "partytown/mod.ts";
+import config from "./fresh.config.ts";
 import manifest from "./fresh.gen.ts";
-import decoManifest from "./manifest.gen.ts";
 
-await start(manifest, {
-  plugins: [
-    ...plugins({
-      manifest: decoManifest,
-    }),
-    partytownPlugin(),
-  ],
-});
+await start(manifest, config);
