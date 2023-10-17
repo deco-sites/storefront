8602c49c3ccf98d12b4d52b5db5d7a6f00c71070
gimenes <tlgimenes@gmail.com>
2023-10-17 10:40:49 -0300

    improve events

--- sdk/analytics.tsx
+++ sdk/analytics.tsx
@@ -1,14 +1,4 @@
 import type { AnalyticsEvent } from "apps/commerce/types.ts";
 
-export const sendEvent = <E extends AnalyticsEvent>(event: E) => {
-  const doSend = window.DECO_SITES_STD &&
-    window.DECO_SITES_STD.sendAnalyticsEvent;
-
-  if (typeof doSend === "function") {
-    return doSend(event);
-  }
-
-  console.info(
-    "Cannot find Analytics section in your page. Press `.` to add Analytics and supress this warning",
-  );
-};
+export const sendEvent = <E extends AnalyticsEvent>(event: E) =>
+  window.DECO.events.dispatch(event);
