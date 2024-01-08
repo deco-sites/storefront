605b73e0a0687164742ddfeb6d7402278bddef77
guitavano <tavano62@gmail.com>
2024-01-08 12:17:45 -0300

    bump apps

--- sections/Content/RichText.tsx
+++ sections/Content/RichText.tsx
@@ -5,9 +5,12 @@ export interface Props {
   containerWidth?: number;
 }
 
-const DEFAULT_TEXT = "<p><span style=\"font-size: 36pt;\" data-mce-style=\"font-size: 36pt;\"><strong>Rich Text</strong></span></p><p><span style=\"font-size: 24pt;\" data-mce-style=\"font-size: 24pt;\"><strong>Rich Text</strong></span></p><p><span style=\"font-size: 18pt;\" data-mce-style=\"font-size: 18pt;\"><strong>Rich Text</strong></span></p><p><span style=\"font-size: 14pt;\" data-mce-style=\"font-size: 14pt;\"><strong>Rich Text</strong></span></p>"
+const DEFAULT_TEXT =
+  '<p><span style="font-size: 36pt;" data-mce-style="font-size: 36pt;"><strong>Rich Text</strong></span></p><p><span style="font-size: 24pt;" data-mce-style="font-size: 24pt;"><strong>Rich Text</strong></span></p><p><span style="font-size: 18pt;" data-mce-style="font-size: 18pt;"><strong>Rich Text</strong></span></p><p><span style="font-size: 14pt;" data-mce-style="font-size: 14pt;"><strong>Rich Text</strong></span></p>';
 
-export default function RichText({ text = DEFAULT_TEXT, containerWidth }: Props) {
+export default function RichText(
+  { text = DEFAULT_TEXT, containerWidth }: Props,
+) {
   return (
     <div
       dangerouslySetInnerHTML={{ __html: text }}
