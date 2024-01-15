d49eb356bc12f5c933c3e61efe10076999a843f3
guitavano <tavano62@gmail.com>
2024-01-15 14:57:00 -0300

    fix: add overflow hidden to cart (#101)

--- components/ui/Drawer.tsx
+++ components/ui/Drawer.tsx
@@ -53,7 +53,7 @@ function Drawer(props: Props) {
         {children}
       </div>
 
-      <aside class="drawer-side h-full z-50">
+      <aside class="drawer-side h-full z-50 overflow-hidden">
         <label for={id} class="drawer-overlay" />
         {!lazy.value && aside}
       </aside>
