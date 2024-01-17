b609f518eb8bfabfab4e0e85b6f18847a05271e9
gimenes <tlgimenes@gmail.com>
2024-01-17 14:50:52 -0300

    bump deps

--- fresh.config.ts
+++ fresh.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "$fresh/server.ts";
-import plugins from "https://denopkg.com/deco-sites/std@4d254969c1e70303115095d5cd7d3912071b9286/plugins/mod.ts";
+import plugins from "https://denopkg.com/deco-sites/std@ad6490d50d377980cd8e9f29efc00634e9eda2cb/plugins/mod.ts";
 import manifest from "./manifest.gen.ts";
 import tailwind from "./tailwind.config.ts";
 
