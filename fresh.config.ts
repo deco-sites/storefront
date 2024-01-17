cd5a0b4728db8255a7aaa03a9e239f0bb809a490
gimenes <tlgimenes@gmail.com>
2024-01-17 15:21:46 -0300

    fresh config

--- fresh.config.ts
+++ fresh.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "$fresh/server.ts";
-import plugins from "https://denopkg.com/deco-sites/std@ad6490d50d377980cd8e9f29efc00634e9eda2cb/plugins/mod.ts";
+import plugins from "https://denopkg.com/deco-sites/std@7ae44269b3c8255f5db3c94c1c07d148eaa84b3f/plugins/mod.ts";
 import manifest from "./manifest.gen.ts";
 import tailwind from "./tailwind.config.ts";
 
