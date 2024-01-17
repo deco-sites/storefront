2b7d6666dffe4ae3a1846cb508ff7b3519e9c25d
gimenes <tlgimenes@gmail.com>
2024-01-17 10:04:48 -0300

    rollback routes app

--- fresh.config.ts
+++ fresh.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "$fresh/server.ts";
-import plugins from "https://denopkg.com/deco-sites/std@7bf6bf5d885211b2e7d171601f4a2c6bddd3abb2/plugins/mod.ts";
+import plugins from "https://denopkg.com/deco-sites/std@7a4ceac0bdf413c6cf8775e62e8ad73f9041ff94/plugins/mod.ts";
 import manifest from "./manifest.gen.ts";
 
 export default defineConfig({
