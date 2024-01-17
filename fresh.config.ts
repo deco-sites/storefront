6d5efd7e9b8530ac1f0693d50e1be3fa4e161fab
gimenes <tlgimenes@gmail.com>
2024-01-17 13:04:09 -0300

    new mode

--- fresh.config.ts
+++ fresh.config.ts
@@ -4,5 +4,9 @@ import manifest from "./manifest.gen.ts";
 import tailwind from "./tailwind.config.ts";
 
 export default defineConfig({
-  plugins: plugins({ manifest}),
+  plugins: plugins({
+    manifest,
+    // deno-lint-ignore no-explicit-any
+    tailwind: tailwind as any,
+  }),
 });
