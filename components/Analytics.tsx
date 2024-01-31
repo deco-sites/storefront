bf1cf10985de4301df07bcc1343d6fd15300b628
igorbrasileiro <brasileiro456@gmail.com>
2024-01-31 15:47:18 -0300

    Change analytics to island

--- components/Analytics.tsx
+++ components/Analytics.tsx
@@ -1,5 +1,5 @@
 import type { AnalyticsEvent } from "apps/commerce/types.ts";
-import { scriptAsDataURI } from "apps/utils/dataURI.ts";
+import { useEffect } from "preact/hooks";
 
 /**
  * This function is usefull for sending events on click. Works with both Server and Islands components
@@ -7,57 +7,51 @@ import { scriptAsDataURI } from "apps/utils/dataURI.ts";
 export const SendEventOnClick = <E extends AnalyticsEvent>({ event, id }: {
   event: E;
   id: string;
-}) => (
-  <script
-    defer
-    src={scriptAsDataURI(
-      (id: string, event: AnalyticsEvent) => {
-        const elem = document.getElementById(id);
+}) => {
+  useEffect(() => {
+    requestAnimationFrame(() => {
+      const elem = document.getElementById(id);
 
-        if (!elem) {
-          return console.warn(
-            `Could not find element ${id}. Click event will not be send. This will cause loss in analytics`,
-          );
-        }
+      if (!elem) {
+        return console.warn(
+          `Could not find element ${id}. Click event will not be send. This will cause loss in analytics`,
+        );
+      }
 
-        elem.addEventListener("click", () => {
-          globalThis.window.DECO.events.dispatch(event);
-        });
-      },
-      id,
-      event,
-    )}
-  />
-);
+      elem.addEventListener("click", () => {
+        globalThis.window.DECO.events.dispatch(event);
+      });
+    });
+  }, []);
+
+  return null;
+};
 
 export const SendEventOnView = <E extends AnalyticsEvent>(
   { event, id }: { event: E; id: string },
-) => (
-  <script
-    defer
-    src={scriptAsDataURI(
-      (id: string, event: E) => {
-        const elem = document.getElementById(id);
+) => {
+  useEffect(() => {
+    requestAnimationFrame(() => {
+      const elem = document.getElementById(id);
 
-        if (!elem) {
-          return console.warn(
-            `Could not find element ${id}. Click event will not be send. This will cause loss in analytics`,
-          );
+      if (!elem) {
+        return console.warn(
+          `Could not find element ${id}. Click event will not be send. This will cause loss in analytics`,
+        );
+      }
+
+      const observer = new IntersectionObserver((items) => {
+        for (const item of items) {
+          if (!item.isIntersecting) continue;
+
+          globalThis.window.DECO.events.dispatch(event);
+          observer.unobserve(elem);
         }
+      });
 
-        const observer = new IntersectionObserver((items) => {
-          for (const item of items) {
-            if (!item.isIntersecting) continue;
+      observer.observe(elem);
+    });
+  }, []);
 
-            globalThis.window.DECO.events.dispatch(event);
-            observer.unobserve(elem);
-          }
-        });
-
-        observer.observe(elem);
-      },
-      id,
-      event,
-    )}
-  />
-);
+  return null;
+};
---
+++ islands/Analytics.tsx
@@ -0,0 +1,1 @@
+export * from "../components/Analytics.tsx";
