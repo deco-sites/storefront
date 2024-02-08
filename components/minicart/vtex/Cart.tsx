db7a65674faaaffb379695378c9f491e77e40d54
Bruno Monteiro <brunobapmon@gmail.com>
2024-02-08 11:41:31 -0300

    fix: vtex discount coming negative

--- components/minicart/vtex/Cart.tsx
+++ components/minicart/vtex/Cart.tsx
@@ -6,7 +6,7 @@ function Cart() {
   const { items, totalizers } = cart.value ?? { items: [] };
   const total = totalizers?.find((item) => item.id === "Items")?.value || 0;
   const discounts =
-    totalizers?.find((item) => item.id === "Discounts")?.value || 0;
+    (totalizers?.find((item) => item.id === "Discounts")?.value || 0) * -1;
   const locale = cart.value?.clientPreferencesData.locale ?? "pt-BR";
   const currency = cart.value?.storePreferencesData.currencyCode ?? "BRL";
   const coupon = cart.value?.marketingData?.coupon ?? undefined;
@@ -32,7 +32,8 @@ function Cart() {
       coupon={coupon}
       onAddCoupon={(text) => addCouponsToCart({ text })}
       onUpdateQuantity={(quantity, index) =>
-        updateItems({ orderItems: [{ index, quantity }] })}
+        updateItems({ orderItems: [{ index, quantity }] })
+      }
       itemToAnalyticsItem={(index) => {
         const item = items[index];
 
