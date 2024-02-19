2244dab56a5433fa8b77bc8373ae974f63560683
gimenes <tlgimenes@gmail.com>
2024-02-19 19:29:33 -0300

    add loading spinners

--- sections/Product/ProductShelfTabbed.tsx
+++ sections/Product/ProductShelfTabbed.tsx
@@ -1,1 +1,9 @@
 export { default } from "$store/components/product/ProductShelfTabbed.tsx";
+
+export function LoadingFallback() {
+  return (
+    <div style={{ height: "716px" }} class="flex justify-center items-center">
+      <span class="loading loading-spinner" />
+    </div>
+  );
+}
