e0596b4ef6cee23bf293e72b53f24855ed8fad7a
caroluchoa <carol_uchoa8@hotmail.com>
2024-02-28 18:25:29 -0300

    update generate icons script to let user add new icons on sprites.svg and reflect on adminIcons.ts

--- static/generate-icons.ts
+++ static/generate-icons.ts
@@ -5,27 +5,46 @@ const outputFilePath = "static/adminIcons.ts";
 
 async function generateIconsFile() {
   const svgContent = await Deno.readTextFile(svgFilePath);
-
-  const regex = /<symbol id="(.+?)"(.+?)>(.+?)<\/symbol>/gs;
-  let match;
-  let svgStrings = '';
-  let iconNames = [];
-
-  while ((match = regex.exec(svgContent)) !== null) {
-    const [fullMatch, id, attributes, content] = match;
-    svgStrings += `export const ${id} = \`<svg id="${id}"${attributes}>${content}</svg>\`;\n`;
-    iconNames.push(id);
+  let existingContent = '';
+  let existingIcons = new Set();
+  let newIcons = [];
+  
+  // Verifies if adminIcons.ts already exists and reads the icons from there
+  if (existsSync(outputFilePath)) {
+    existingContent = await Deno.readTextFile(outputFilePath);
+    const regexExtract = /export const (\w+) =/g;
+    let matchExtract;
+    while ((matchExtract = regexExtract.exec(existingContent)) !== null) {
+      existingIcons.add(matchExtract[1]);
+    }
   }
 
-  const outputFileContent = `${svgStrings}\nexport const AvailableIcons = { ${iconNames.join(', ')} };`;
+  const regex = /<symbol id="(.+?)"(.+?)>(.+?)<\/symbol>/gs;
+  let matchSymbol;
+  while ((matchSymbol = regex.exec(svgContent)) !== null) {
+    const [_, id, attributes, content] = matchSymbol;
+    // Adds only new icons
+    if (!existingIcons.has(id)) {
+      newIcons.push(id);
+      const iconString = `export const ${id} = \`<svg id="${id}"${attributes}>${content}</svg>\`;\n`;
+      existingContent += iconString;
+    }
+  }
 
-  await Deno.writeTextFile(outputFilePath, outputFileContent);
+  // If there are new icons, update AvailableIcons and the file
+  if (newIcons.length > 0) {
+    // Remove the existing AvailableIcons constant from the content
+    existingContent = existingContent.replace(/export const AvailableIcons = { [^}]* };/g, '');
+    // Adds all existing icons plus the new ones to the AvailableIcons constant
+    const allIcons = [...existingIcons, ...newIcons];
+    const availableIconsString = `export const AvailableIcons = { ${allIcons.join(', ')} };`;
+    existingContent += `\n${availableIconsString}`;
 
-  console.log('Icons as string file generated successfully.');
+    await Deno.writeTextFile(outputFilePath, existingContent, { create: true });
+    console.log('adminIcons.ts updated successfully with new icons.');
+  } else {
+    console.log('No new icons to add.');
+  }
 }
 
-if (existsSync(outputFilePath)) {
-  console.log('Icons as string file already exists. Skipping generation.');
-} else {
-  generateIconsFile().catch(console.error);
-}
+generateIconsFile().catch(console.error);
