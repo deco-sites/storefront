230d42eb0da9d89210651cf02e9eb81657a50309
igorbrasileiro <brasileiro456@gmail.com>
2024-06-12 13:36:40 -0300

    Animate view-transition

--- components/search/SearchResult.tsx
+++ components/search/SearchResult.tsx
@@ -227,7 +227,11 @@ function Result(props: SectionProps<typeof loader>) {
 
   return (
     <>
-      <div id={id} {...viewItemListEvent}>
+      <div
+        id={id}
+        {...viewItemListEvent}
+        class="[view-transition-name:fallback]"
+      >
         {partial
           ? <PageResult {...props} />
           : (
--- sections/Product/SearchResult.tsx
+++ sections/Product/SearchResult.tsx
@@ -2,7 +2,10 @@ export { default, loader } from "../../components/search/SearchResult.tsx";
 
 export function LoadingFallback() {
   return (
-    <div style={{ height: "100vh" }} class="flex justify-center items-center">
+    <div
+      style={{ height: "100vh" }}
+      class="flex justify-center items-center [view-transition-name:fallback]"
+    >
       <span class="loading loading-spinner" />
     </div>
   );
