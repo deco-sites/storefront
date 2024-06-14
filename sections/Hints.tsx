be7ac03cb2e5f275d81a3f53d58fc65cbc79624e
igorbrasileiro <brasileiro456@gmail.com>
2024-06-14 18:14:13 -0300

    Revert "Preload google fonts directly"
    This reverts commit 1876eb0445d190d6e01c585c0c61ff50227a3dc1.

--- sections/Hints.tsx
+++ sections/Hints.tsx
@@ -2,12 +2,12 @@ import { AppContext } from "../apps/site.ts";
 
 export function loader(_: null, __: Request, ctx: AppContext) {
   const fonts = [
-    "https://fonts.gstatic.com/s/intertight/v7/NGSwv5HMAFg6IuGlBNMjxLsH8ahuQ2e8.woff2",
+    "/live/invoke/website/loaders/asset.ts?src=https://fonts.gstatic.com/s/intertight/v7/NGSwv5HMAFg6IuGlBNMjxLsH8ahuQ2e8.woff2",
   ];
   fonts.forEach((font) => {
     ctx.response.headers.append(
       "link",
-      `<${font}>; rel=preload; as=font; crossorigin`,
+      `<${font}>; rel=preload; as=font; type=font/woff2`,
     );
   });
 }
