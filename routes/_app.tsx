140406f94bea7d181c6600bbd4483e1980565b06
igorbrasileiro <brasileiro456@gmail.com>
2024-06-17 17:15:58 -0300

    Add webvitals script

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -2,6 +2,29 @@ import { asset, Head } from "$fresh/runtime.ts";
 import { defineApp } from "$fresh/server.ts";
 import { useScript } from "apps/utils/useScript.ts";
 import { Context } from "deco/deco.ts";
+import type { onINP } from "https://esm.sh/web-vitals@4.1.1";
+
+type INPHandler = Parameters<typeof onINP>[0];
+
+const sendWebVitalsReport = (v: unknown) => {
+  // suggestion:  https://github.com/GoogleChrome/web-vitals?tab=readme-ov-file#report-only-the-delta-of-changes
+  console.log(v);
+};
+
+const reportFn: INPHandler = (
+  report,
+) => {
+  const { entries, ...otherProps } = report;
+  const lastEntry = entries?.[entries.length - 1].toJSON();
+  lastEntry.element = {
+    element: lastEntry.target?.nodeName,
+    class: (lastEntry.target as undefined | HTMLElement)?.classList.toString(),
+  };
+  sendWebVitalsReport({
+    ...otherProps,
+    lastEntry,
+  });
+};
 
 const serviceWorkerScript = () =>
   addEventListener("load", () =>
@@ -30,6 +53,17 @@ export default defineApp(async (_req, ctx) => {
 
         {/* Web Manifest */}
         <link rel="manifest" href={asset("/site.webmanifest")} />
+
+        <script
+          type="module"
+          dangerouslySetInnerHTML={{
+            __html: `
+          import {onINP} from "https://esm.sh/web-vitals@4.1.1";
+          globalThis.window.sendWebVitalsReport = globalThis.window.sendWebVitalsReport || (${sendWebVitalsReport});
+          onINP(${reportFn}, {reportAllChanges: true});
+        `,
+          }}
+        />
       </Head>
 
       {/* Rest of Preact tree */}
