95b904277e40db980713ace771365e1e4259be2a
gimenes <tlgimenes@gmail.com>
2024-06-21 16:24:05 -0300

    fix gallery styles

--- components/product/Gallery.tsx
+++ components/product/Gallery.tsx
@@ -45,77 +45,80 @@ export default function GallerySlider(props: Props) {
 
   return (
     <>
-      <div id={id} class="flex flex-col sm:flex-row-reverse gap-5">
+      <div id={id} class="grid grid-flow-row sm:grid-flow-col gap-5">
         {/* Image Slider */}
-        <div class="relative h-min flex-grow">
-          <Slider class="carousel carousel-center gap-6 w-full">
-            {images.map((img, index) => (
-              <Slider.Item
-                index={index}
-                class="carousel-item w-full"
-              >
-                <Image
-                  class="w-full"
-                  sizes="(max-width: 640px) 100vw, 40vw"
-                  style={{ aspectRatio: ASPECT_RATIO }}
-                  src={img.url!}
-                  alt={img.alternateName}
-                  width={WIDTH}
-                  height={HEIGHT}
-                  // Preload LCP image for better web vitals
-                  preload={index === 0}
-                  loading={index === 0 ? "eager" : "lazy"}
-                />
-              </Slider.Item>
-            ))}
-          </Slider>
+        <div class="col-start-1 col-span-1 sm:col-start-2">
+          <div class="relative h-min flex-grow">
+            <Slider class="carousel carousel-center gap-6 w-full">
+              {images.map((img, index) => (
+                <Slider.Item
+                  index={index}
+                  class="carousel-item w-full"
+                >
+                  <Image
+                    class="w-full"
+                    sizes="(max-width: 640px) 100vw, 40vw"
+                    style={{ aspectRatio: ASPECT_RATIO }}
+                    src={img.url!}
+                    alt={img.alternateName}
+                    width={WIDTH}
+                    height={HEIGHT}
+                    // Preload LCP image for better web vitals
+                    preload={index === 0}
+                    loading={index === 0 ? "eager" : "lazy"}
+                  />
+                </Slider.Item>
+              ))}
+            </Slider>
 
-          <Slider.PrevButton
-            class="no-animation absolute left-2 top-1/2 btn btn-circle btn-outline disabled:invisible"
-            disabled
-          >
-            <Icon id="chevron-right" class="rotate-180" />
-          </Slider.PrevButton>
+            <Slider.PrevButton
+              class="no-animation absolute left-2 top-1/2 btn btn-circle btn-outline disabled:invisible"
+              disabled
+            >
+              <Icon id="chevron-right" class="rotate-180" />
+            </Slider.PrevButton>
 
-          <Slider.NextButton
-            class="no-animation absolute right-2 top-1/2 btn btn-circle btn-outline disabled:invisible"
-            disabled={images.length < 2}
-          >
-            <Icon id="chevron-right" />
-          </Slider.NextButton>
+            <Slider.NextButton
+              class="no-animation absolute right-2 top-1/2 btn btn-circle btn-outline disabled:invisible"
+              disabled={images.length < 2}
+            >
+              <Icon id="chevron-right" />
+            </Slider.NextButton>
 
-          <div class="absolute top-2 right-2 bg-base-100 rounded-full">
-            <label class="btn btn-ghost hidden sm:inline-flex" for={zoomId}>
-              <Icon id="pan_zoom" />
-            </label>
+            <div class="absolute top-2 right-2 bg-base-100 rounded-full">
+              <label class="btn btn-ghost hidden sm:inline-flex" for={zoomId}>
+                <Icon id="pan_zoom" />
+              </label>
+            </div>
           </div>
         </div>
 
         {/* Dots */}
-        <ul
-          class={clx(
-            "carousel carousel-center",
-            "sm:carousel-vertical",
-            "gap-1 px-4",
-            "sm:gap-2 sm:px-0",
-          )}
-          style={{ maxHeight: "600px" }}
-        >
-          {images.map((img, index) => (
-            <li class="carousel-item">
-              <Slider.Dot index={index}>
-                <Image
-                  style={{ aspectRatio: "1 / 1" }}
-                  class="group-disabled:border-base-300 border rounded object-cover"
-                  width={60}
-                  height={60}
-                  src={img.url!}
-                  alt={img.alternateName}
-                />
-              </Slider.Dot>
-            </li>
-          ))}
-        </ul>
+        <div class="col-start-1 col-span-1">
+          <ul
+            class={clx(
+              "carousel carousel-center",
+              "sm:carousel-vertical",
+              "gap-2",
+            )}
+            style={{ maxHeight: "600px" }}
+          >
+            {images.map((img, index) => (
+              <li class="carousel-item w-16 h-16">
+                <Slider.Dot index={index}>
+                  <Image
+                    style={{ aspectRatio: "1 / 1" }}
+                    class="group-disabled:border-base-300 border rounded object-cover w-full h-full"
+                    width={64}
+                    height={64}
+                    src={img.url!}
+                    alt={img.alternateName}
+                  />
+                </Slider.Dot>
+              </li>
+            ))}
+          </ul>
+        </div>
 
         <Slider.JS rootId={id} />
       </div>
