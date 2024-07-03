440c9c506b88088232b6bdf8245bd5f8479783bd
Tiago Gimenes <gimenes@deco.cx>
2024-07-03 00:23:38 +0000

    Fix lots of skus on product card

--- components/product/ProductCard.tsx
+++ components/product/ProductCard.tsx
@@ -173,7 +173,7 @@ function ProductCard({
 
       {/* SKU Selector */}
       {variants.length > 1 && (
-        <ul class="flex items-center justify-start gap-2 pt-4 pb-1 pl-1">
+        <ul class="flex items-center justify-start gap-2 pt-4 pb-1 pl-1 overflow-x-auto">
           {variants.map(([value, link]) => [value, relative(link)] as const)
             .map(([value, link]) => (
               <li>
