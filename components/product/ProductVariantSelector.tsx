68ebe5868319e3619318f0f16084e9450b38911c
Tiago Gimenes <gimenes@deco.cx>
2024-07-04 19:21:48 +0000

    Release staging environment with 3 changes made by Muriel Dias, Tiago Gimenes

--- components/product/ProductVariantSelector.tsx
+++ components/product/ProductVariantSelector.tsx
@@ -14,6 +14,12 @@ const colors: Record<string, string | undefined> = {
   "Black": "black",
   "Gray": "gray",
   "Blue": "#99CCFF",
+  "AzulMarinho": "#4E6E95",
+  "AzulClaro": "#bedae4",
+  "VerdeEscuro": "#446746",
+  "VerdeClaro": "#aad1b5",
+  "AmareloEscuro": "#c6b343",
+  "AmareloClaro": "#F1E8B0 ",
 };
 
 const useStyles = (value: string, checked: boolean) => {
