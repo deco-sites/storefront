bae087aeb721a50671c62e78adc8b3fbfb096f92
Marcos Candeia <marrcooos@gmail.com>
2024-07-26 20:22:01 -0300

    Exit on build
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- dev.ts
+++ dev.ts
@@ -3,6 +3,10 @@ import { DecoRouteState, setup } from "deco/mod.ts";
 import { Hono } from "@hono/hono";
 import type { Manifest } from "./manifest.gen.ts";
 
+if (Deno.args.includes("build")) {
+  Deno.exit(0);
+}
+
 const honoApp = new Hono<DecoRouteState<Manifest>>();
 await setup(honoApp);
 const port = Deno.env.get("PORT") ? +Deno.env.get("PORT")! : 8000;
