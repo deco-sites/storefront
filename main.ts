5d8ac94a194d9ad81747553c5d6b0ef1bea2a337
Marcos Candeia <marrcooos@gmail.com>
2024-07-26 20:19:23 -0300

    Use dev server as hono
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- main.ts
+++ main.ts
@@ -1,10 +1,1 @@
-/// <reference no-default-lib="true"/>
-/// <reference lib="dom" />
-/// <reference lib="deno.ns" />
-/// <reference lib="esnext" />
-
-import { start } from "$fresh/server.ts";
-import config from "./fresh.config.ts";
-import manifest from "./fresh.gen.ts";
-
-await start(manifest, config);
+import "./dev.ts";
