078e431950549210caecfa40ab62c681782a4ee7
Marcos Candeia <marrcooos@gmail.com>
2024-08-04 20:44:45 -0300

    Use deco/apps version that uses hono
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- _app.tsx
+++ _app.tsx
@@ -4,47 +4,48 @@ import { Head } from "deco/runtime/htmx/Renderer.tsx";
 import { ComponentChildren } from "preact";
 
 const serviceWorkerScript = () =>
-    addEventListener("load", () =>
-        navigator && navigator.serviceWorker &&
-        navigator.serviceWorker.register("/sw.js?__frsh_c"));
+  addEventListener("load", () =>
+    navigator && navigator.serviceWorker &&
+    navigator.serviceWorker.register("/sw.js?__frsh_c"));
 
 export const Layout = (
-    { children, revision, hmrUniqueId }: {
-        children: ComponentChildren;
-        revision: string;
-        hmrUniqueId: string;
-    },
+  { children, revision, hmrUniqueId }: {
+    children: ComponentChildren;
+    revision: string;
+    hmrUniqueId: string;
+  },
 ) => {
-    return (
-        <>
-            {/* Include Icons and manifest */}
-            <Head>
-                {/* Enable View Transitions API */}
-                <style
-                    dangerouslySetInnerHTML={{
-                        __html: `@view-transition { navigation: auto; }`,
-                    }}
-                />
+  return (
+    <>
+      {/* Include Icons and manifest */}
+      {/** @ts-ignore: ignore error */}
+      <Head>
+        {/* Enable View Transitions API */}
+        <style
+          dangerouslySetInnerHTML={{
+            __html: `@view-transition { navigation: auto; }`,
+          }}
+        />
 
-                {/* Tailwind v3 CSS file */}
-                <link
-                    href={`/styles.css?revision=${revision}${hmrUniqueId}`}
-                    rel="stylesheet"
-                />
+        {/* Tailwind v3 CSS file */}
+        <link
+          href={`/styles.css?revision=${revision}${hmrUniqueId}`}
+          rel="stylesheet"
+        />
 
-                {/* Web Manifest */}
-                <link rel="manifest" href={asset("/site.webmanifest")} />
-            </Head>
+        {/* Web Manifest */}
+        <link rel="manifest" href={asset("/site.webmanifest")} />
+      </Head>
 
-            {/* Rest of Preact tree */}
-            {children}
+      {/* Rest of Preact tree */}
+      {children}
 
-            <script
-                type="module"
-                dangerouslySetInnerHTML={{
-                    __html: useScript(serviceWorkerScript),
-                }}
-            />
-        </>
-    );
+      <script
+        type="module"
+        dangerouslySetInnerHTML={{
+          __html: useScript(serviceWorkerScript),
+        }}
+      />
+    </>
+  );
 };
