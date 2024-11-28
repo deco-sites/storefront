23de987b207d6f3a30f2d0f09ec6abe6223479fa
gabrielMatosBoubee <gabriel.boubee@carcara.tech>
2024-11-28 20:24:05 -0300

    chore: test

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,7 @@
-// const { defineConfig } = require("cypress");
+import { defineConfig } from 'cypress'
 
-// module.exports = defineConfig({
-//   e2e: {
-//     setupNodeEvents(on, config) {
-//       // implement node event listeners here
-//     },
-//   },
-// });
+export default defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:8000',
+  },
+})
