13509d7d022f8e7da785e61564811a732566b305
guitavano <tavano62@gmail.com>
2025-07-09 09:51:38 -0300

    storefront as a mcp by default

--- fresh.config.ts
+++ fresh.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from "$fresh/server.ts";
 import { plugins } from "deco/plugins/deco.ts";
 import manifest from "./manifest.gen.ts";
+import { mcpServer } from "@deco/mcp";
 
 export default defineConfig({
   plugins: plugins({
     manifest,
     htmx: true,
+    useServer: (deco, hono) => {
+      hono.use("/*", mcpServer(deco));
+    },
   }),
 });
